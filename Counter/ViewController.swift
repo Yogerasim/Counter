133264b850df6c602e17e5fa95a0973b4d904756
yogerrasim@yandex.ru <74001774+Yogerasim@users.noreply.github.com>
2025-03-25 10:04:02 +0300

    add buttons and count

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    private var counter = 0
+    private func updateLabel() {
+            labelCount.text = "\(counter)"
+        }
     override func viewDidLoad() {
         super.viewDidLoad()
+        labelCount.text = "0"
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonDidTab(_ sender: Any) {
+        counter += 1
+        labelCount.text = "\(counter)"
+    }
+    @IBOutlet weak var labelCount: UILabel!
+    
+    @IBAction func buttonPlusTab(_ sender: Any) {
+        counter += 1
+                updateLabel()
+    }
+    @IBAction func buttonMinusTab(_ sender: Any) {
+        if counter > 0 {
+                    counter -= 1
+                    updateLabel()
+                }
+    }
 }
 
